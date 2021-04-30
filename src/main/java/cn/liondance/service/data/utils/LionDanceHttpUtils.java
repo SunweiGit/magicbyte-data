3288018afa4748565f974bc042bd29f1abd81852
sunwei <zhi1278300799@icloud.com>
2021-04-30 09:27:50 +0800

    add LionDanceHttpUtils

---
+++ src/main/java/cn/liondance/service/data/utils/LionDanceHttpUtils.java
@@ -0,0 +1,209 @@
+package cn.liondance.service.data.utils;
+
+import com.google.common.collect.Maps;
+import io.swagger.models.HttpMethod;
+import lombok.Builder;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * The type Lion dance http utils.
+ *
+ * @author sunwei
+ */
+@Builder
+public final class LionDanceHttpUtils {
+
+    /**
+     * Get string.
+     *
+     * @param url     the url
+     * @param headers the headers
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String get(String url, Map<String, String> headers) throws IOException {
+        return newCall(HttpMethod.GET, url, headers, null);
+    }
+
+    /**
+     * Get string.
+     *
+     * @param url the url
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String get(String url) throws IOException {
+        return newCall(HttpMethod.GET, url, Maps.newHashMap(), null);
+    }
+
+    /**
+     * Head string.
+     *
+     * @param url     the url
+     * @param headers the headers
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String head(String url, Map<String, String> headers) throws IOException {
+        return newCall(HttpMethod.HEAD, url, headers, null);
+    }
+
+
+    /**
+     * Head string.
+     *
+     * @param url the url
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String put(String url) throws IOException {
+        return newCall(HttpMethod.PUT, url, Maps.newHashMap(), null);
+    }
+
+
+    /**
+     * Post string.
+     *
+     * @param url         the url
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String post(String url, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.POST, url, Maps.newHashMap(), requestBody);
+    }
+
+    /**
+     * Post string.
+     *
+     * @param url         the url
+     * @param headers     the headers
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String post(String url, Map<String, String> headers, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.POST, url, headers, requestBody);
+    }
+
+    /**
+     * Put string.
+     *
+     * @param url         the url
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String put(String url, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.PUT, url, Maps.newHashMap(), requestBody);
+    }
+
+    /**
+     * Put string.
+     *
+     * @param url         the url
+     * @param headers     the headers
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String put(String url, Map<String, String> headers, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.PUT, url, headers, requestBody);
+    }
+
+    /**
+     * Patch string.
+     *
+     * @param url         the url
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String patch(String url, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.PATCH, url, Maps.newHashMap(), requestBody);
+    }
+
+    /**
+     * Patch string.
+     *
+     * @param url         the url
+     * @param headers     the headers
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String patch(String url, Map<String, String> headers, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.PATCH, url, headers, requestBody);
+    }
+
+    /**
+     * Delete string.
+     *
+     * @param url         the url
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String delete(String url, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.DELETE, url, Maps.newHashMap(), requestBody);
+    }
+
+    /**
+     * Delete string.
+     *
+     * @param url         the url
+     * @param headers     the headers
+     * @param requestBody the request body
+     * @return the string
+     * @throws IOException the io exception
+     */
+    public static String delete(String url, Map<String, String> headers, RequestBody requestBody) throws IOException {
+        return newCall(HttpMethod.DELETE, url, headers, requestBody);
+    }
+
+
+    private static String newCall(HttpMethod method, String url, Map<String, String> headers, RequestBody requestBody) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request.Builder builder = new Request.Builder().url(url);
+        if (!headers.isEmpty()) {
+            builder.headers(Headers.of(headers));
+        }
+        switch (method) {
+            case POST:
+                builder.post(requestBody);
+                break;
+            case PUT:
+                builder.put(requestBody);
+                break;
+            case PATCH:
+                builder.patch(requestBody);
+                break;
+            case DELETE:
+                builder.delete(requestBody);
+                break;
+            case HEAD:
+                builder.head();
+                break;
+            case GET:
+            default:
+                builder.get();
+                break;
+        }
+        try (Response response = client.newCall(builder.build()).execute()) {
+            if (!response.isSuccessful()) {
+                // ... handle failed request
+            }
+            String responseBody = response.body().string();
+            // ... do something with response
+            return responseBody;
+        } catch (IOException e) {
+            // ... handle IO exception
+            throw e;
+        }
+    }
+
+
+}
